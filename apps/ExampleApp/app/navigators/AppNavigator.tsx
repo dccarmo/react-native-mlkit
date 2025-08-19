ff8c3a5e079d284edf88c615f893ca8383b37947
Diogo Carmo <dccarmo@gmail.com>
2025-08-19 18:27:30 -0300

    Revert "feat: added initial support for text recognition on iOS"
    This reverts commit 1aa93beae191ba296a44c57d98215f6ea6ceab14.

--- apps/ExampleApp/app/navigators/AppNavigator.tsx
+++ apps/ExampleApp/app/navigators/AppNavigator.tsx
@@ -35,7 +35,6 @@ export type AppStackParamList = {
   ImageLabeling: Record<string, never>
   ObjectDetection: Record<string, never>
   DocumentScanner: Record<string, never>
-  TextRecognition: Record<string, never>
   // IGNITE_GENERATOR_ANCHOR_APP_STACK_PARAM_LIST
 }
 
@@ -69,7 +68,6 @@ const AppStack = observer(function AppStack() {
       <Stack.Screen name="ImageLabeling" component={Screens.ImageLabelingScreen} />
       <Stack.Screen name="ObjectDetection" component={Screens.ObjectDetectionScreen} />
       <Stack.Screen name="DocumentScanner" component={Screens.DocumentScannerScreen} />
-      <Stack.Screen name="TextRecognition" component={Screens.TextRecognitionScreen} />
       {/* IGNITE_GENERATOR_ANCHOR_APP_STACK_SCREENS */}
     </Stack.Navigator>
   )
--- apps/ExampleApp/app/utils/useExampleImage/useExampleImage.ts
+++ apps/ExampleApp/app/utils/useExampleImage/useExampleImage.ts
@@ -3,6 +3,7 @@ import {
   launchCameraAsync,
   launchImageLibraryAsync,
   ImagePickerOptions,
+  MediaTypeOptions,
   ImagePickerAsset,
   useCameraPermissions,
 } from "expo-image-picker"
@@ -16,7 +17,6 @@ import {
   imageFilters,
   imageGroupers,
 } from "./examples"
-import { launchImageLibrary } from "react-native-image-picker"
 
 export type UseExampleImageStatus =
   | "init"
@@ -56,7 +56,7 @@ interface ZippedImage extends RandomImage {
 }
 
 const IMAGE_PICKER_OPTIONS: ImagePickerOptions = {
-  mediaTypes: "images",
+  mediaTypes: MediaTypeOptions.Images,
   allowsEditing: false,
   quality: 0.5,
 }
@@ -127,10 +127,7 @@ export function useExampleImage(predicates?: {
       return
     }
     setStatus("takingPhoto")
-    // const result = await launchCameraAsync(IMAGE_PICKER_OPTIONS)
-    const result = await launchImageLibrary({
-      mediaType: "photo",
-    })
+    const result = await launchCameraAsync(IMAGE_PICKER_OPTIONS)
     if (result.assets?.[0]) {
       setImage({ ...result.assets?.[0], localUri: result.assets?.[0].uri } as SelectedImage)
     } else {
